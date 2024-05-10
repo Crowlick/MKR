e13011dc389031c2931da8eeed58a6f942017f6e
Crowlick <ivan-kochet0v@mailru>
2024-05-10 19:38:44 +0300

    Modifying parameters

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ void visualize(std::ofstream &file, std::string filename) {
 
 int main()
 {
-	double w = 500.;
+	double w = 500.; // Geometry parameters
 	double h = 400.;
 	double S = 150.;
 	double R1 = 150.;
@@ -23,31 +23,35 @@ int main()
 	double XS2 = 355.;
 	double YS2 = 255.;
 	
+	int l = 1; //Bound conditions
+	int t = 1;
+	int r = 1;
+	int b = 1;
+	int r1 = 1;
+	int r2 = 1;
+	int s = 1;
+	
+	double step = 5.; // Mesh step
+	
 	std::map<std::string, double> base{{"a", w / 2}, {"b", h / 2}, {"h_x", 1/ w}, {"h_y", 1/ h}};
 	std::map<std::string, double> sqr{{"a", XS2}, {"b", YS2}, {"h_x", 1 / S}, {"h_y", 1 / S}};
 	std::map<std::string, double> circle{{"a", XS1}, {"b", YS1}, {"h_x", 1 / R2}, {"h_y", 1 / R2}};
 	std::map<std::string, double> arc{{"a", w - R1}, {"b", h - R1}, {"h_x", 1 / R1}, {"h_y", 1 / R1}};
 	
 	Object obj;
-	obj.Add_Form("Rectangle", sqr, true, 2);
-	obj.Add_Form("Circle", circle, true, 3);
-	obj.Add_Form("Arc", arc, true, 1);
+	obj.Add_Form("Rectangle", sqr, true, s);
+	obj.Add_Form("Circle", circle, true, r2);
+	obj.Add_Form("Arc", arc, true, r1);
 	obj.Add_Form("Rectangle", base, false, 1);
 	
-	double step = 5.;
-	std::ofstream file("data.dat");
 	std::ofstream script("script.plt");
 	
 	System sys(obj, step);
-	sys.DefineBounds(1, 1, 1, 1);
+	sys.DefineBounds(l, r, t, b);
 	
-	Solver s("explicit.dat", "implicit.dat", 1.);
-	s.SolveImplicit(sys, 1000.);
-	for (auto line : sys.Nodes())
-		for (auto node : line)
-			file << node->X() << ' ' << node->Y() << ' ' << node->T() << '\n';
-	file.close();
-	system("python3 vis.py");
+	Solver slv("explicit.dat", "implicit.dat", 1.);
+	slv.SolveImplicit(sys, 1000.);
+
 	visualize(script, "implicit.dat");
 	system("gnuplot script.plt");
 	return 0;
